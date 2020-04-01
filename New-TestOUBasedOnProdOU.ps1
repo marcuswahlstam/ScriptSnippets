d22cb50e053e6ad6431a1ff6a293a11c792356fa
Marcus Wahlstam <marcus@wahlstam.se>
2020-04-01 15:16:53 +0200

    Update New-TestOUBasedOnProdOU.ps1

--- New-TestOUBasedOnProdOU.ps1
+++ New-TestOUBasedOnProdOU.ps1
@@ -1,10 +1,10 @@
-$prodOUs = Get-ADOrganizationalUnit -Filter * -SearchBase "OU=Koncern root,DC=kahrs,DC=com" -SearchScope OneLevel
+$prodOUs = Get-ADOrganizationalUnit -Filter * -SearchBase "OU=Prod,DC=domain,DC=com" -SearchScope OneLevel
 
 foreach ($ou in $prodOUs)
 {
     $ouName = $ou.Name
     $ouDN = $ou.DistinguishedName
-    $ouDNTest = $ouDN -replace 'OU=Koncern root,DC=kahrs,DC=com','OU=Koncern root - Test,DC=kahrs,DC=com'
+    $ouDNTest = $ouDN -replace 'OU=Prod,DC=domain,DC=com','OU=Test,DC=domain,DC=com'
     "$ouDNTest"
     New-ADOrganizationalUnit -Path 
 }
